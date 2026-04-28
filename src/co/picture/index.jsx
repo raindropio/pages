9e1c2354091c301ce084245d3394c6e4c5ccfb22
exentrich <winlinx@yandex.com>
2026-04-28 16:53:31 +0300

    remove avif, slower

--- src/co/picture/index.jsx
+++ src/co/picture/index.jsx
@@ -22,7 +22,6 @@ export default function Image({ className='', src, width, height, ar, mode, alt,
         <picture 
             className={s.picture + ' ' + className}
             style={{ ...arStyle, width, height }}>
-            <source srcSet={`${link}&format=avif&dpr=1 1x, ${link}&format=avif&dpr=2 2x`} type='image/avif' />
             <source srcSet={`${link}&dpr=1 1x, ${link}&dpr=2 2x`} />
 
             <img 
